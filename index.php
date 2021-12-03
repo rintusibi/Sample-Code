43db424fcc4de7d4e40e3b764fab0cf80f290464
Rintu Thomas <rintu_t@yatnam.co.in>
2021-12-03 20:31:01 +0530

    Comment removed

--- index.php
+++ index.php
@@ -25,14 +25,12 @@ get_header(); ?>
 <?php
 if ( have_posts() ) {
 
-	// Load posts loop.
 	while ( have_posts() ) {
 		the_post();
 
 		get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) );
 	}
 
-	// Previous/next page navigation.
 	twenty_twenty_one_the_posts_navigation();
 
 } else {
